ffb7642fa90d2cb479c0cb363fa219ae107393ae
Comunidad de Odoo Ecuador <damendieta@libre.ec>
2016-01-25 09:46:50 -0500

    Update tarjeta.py

--- tarjeta.py
+++ tarjeta.py
@@ -1,1 +1,7 @@
 def _fabrica_de_software(libre):
+  nombre = Daniel Alejandro Mendieta
+  función = Gerente General
+  correo = damendieta@libre.ec
+  celular = movistar(09966577156)
+            claro(0996601902)
+  
